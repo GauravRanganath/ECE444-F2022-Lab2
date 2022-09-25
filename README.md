b09d80f0a83cbfe24299a9ace3750fb09826db15
Gaurav Ranganath <gaurav.ranganath@mail.utoronto.ca>
2022-09-24 20:40:52 -0400

    finish activity 4

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 Gaurav Ranganath
 
 This repo is a clone of https://github.com/miguelgrinberg/flasky.
+
+## Activity 1 ##
+
+![Commit Screenshot](/activity_1.jpg "Commit Screenshot")
+
+## Activity 2 ##
+
+![Commit Screenshot](/activity_2.jpg "Commit Screenshot")
+
+## Activity 3 ##
+
+![Commit Screenshot](/activity_3.jpg "Commit Screenshot")
+
+## Activity 4 ##
+
+![Commit Screenshot](/activity_4_1.jpg "Commit Screenshot")
+![Commit Screenshot](/activity_4_2.jpg "Commit Screenshot")
+![Commit Screenshot](/activity_4_3.jpg "Commit Screenshot")
+![Commit Screenshot](/activity_4_4.jpg "Commit Screenshot")
--- hello.py
+++ hello.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    utmail = EmailField('What is your UofT Email address?',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_utmail = session.get('utmail')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_utmail is not None and old_utmail != form.utmail.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['utmail'] = form.utmail.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), utmail=session.get('utmail'))
